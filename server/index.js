d83f06be4823d28d8d71ca6c44370554c3bdf116
Alex Kinsey <alex.kinsey@lloydsbanking.com>
2022-06-15 16:41:53 +0100

    CRUD complete for customer

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
+
 // connect to concierge DB
 const db = new sqlite3.Database(__dirname + '/database/concierge.db');
 
+// set up app
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// assign port
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
@@ -15,8 +18,8 @@ app.listen(PORT, () => {
 
 // GET all customers
 app.get('/api/customers', (req, res, next) => {
-  var sql = 'SELECT * FROM customers';
-  var params = [];
+  const sql = 'SELECT * FROM customers';
+  const params = [];
   db.all(sql, params, (err, rows) => {
     if (err) {
       res.status(400).json({ error: err.message });
@@ -45,8 +48,8 @@ app.get('/api/customers/:id', (req, res, next) => {
   });
 });
 
+// CREATE customer
 app.post('/api/customers/', (req, res, next) => {
-  console.log(req.body);
   const errors = [];
   if (!req.body.first_name) {
     errors.push('No first name specified');
@@ -61,6 +64,7 @@ app.post('/api/customers/', (req, res, next) => {
     res.status(400).json({ error: errors.join(',') });
     return;
   }
+
   const data = {
     first_name: req.body.first_name,
     last_name: req.body.last_name,
@@ -74,8 +78,19 @@ app.post('/api/customers/', (req, res, next) => {
     phone_number: req.body.phone_number,
     national_insurance_number: req.body?.national_insurance_number
   };
-  const sql =
-    'INSERT INTO customers (first_name, last_name, date_of_birth, address1, address2, address3, city, postcode, email, phone_number, national_insurance_number) VALUES (?,?,?,?,?,?,?,?,?,?,?)';
+  const sql = `INSERT INTO customers 
+                (first_name, 
+                  last_name, 
+                  date_of_birth, 
+                  address1, 
+                  address2, 
+                  address3, 
+                  city, 
+                  postcode, 
+                  email, 
+                  phone_number, 
+                  national_insurance_number) 
+              VALUES (?,?,?,?,?,?,?,?,?,?,?)`;
   const params = [
     data.first_name,
     data.last_name,
@@ -101,4 +116,76 @@ app.post('/api/customers/', (req, res, next) => {
       id: this.lastID
     });
   });
+});
+
+// UPDATE customer
+app.put('/api/customers/:id', (req, res, next) => {
+  console.log(req.body);
+  const data = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    date_of_birth: req.body.date_of_birth,
+    address1: req.body.address1,
+    address2: req.body?.address2,
+    address3: req.body?.address3,
+    city: req.body.city,
+    postcode: req.body.postcode,
+    email: req.body.email,
+    phone_number: req.body.phone_number,
+    national_insurance_number: req.body?.national_insurance_number
+  };
+  const sql = `UPDATE customers SET
+                first_name = COALESCE(?,first_name),
+                last_name = COALESCE(?,last_name),
+                date_of_birth = COALESCE(?,date_of_birth),
+                address1 = COALESCE(?,address1),
+                address2 = COALESCE(?,address2),
+                address3 = COALESCE(?,address3),
+                city = COALESCE(?,city),
+                postcode = COALESCE(?,postcode),
+                email = COALESCE(?,email),
+                phone_number = COALESCE(?,phone_number),
+                national_insurance_number = COALESCE(?,national_insurance_number)
+              WHERE customer_id = ?`;
+  const params = [
+    data.first_name,
+    data.last_name,
+    data.date_of_birth,
+    data.address1,
+    data.address2,
+    data.address3,
+    data.city,
+    data.postcode,
+    data.email,
+    data.phone_number,
+    data.national_insurance_number,
+    req.params.id
+  ];
+
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: data,
+      changes: this.changes
+    });
+  });
+});
+
+// DELETE customer
+app.delete('/api/customers/:id', (req, res, next) => {
+  db.run(
+    'DELETE FROM customers WHERE customer_id = ?',
+    req.params.id,
+    function (err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+      res.json({ message: 'deleted', changes: this.changes });
+    }
+  );
 });
