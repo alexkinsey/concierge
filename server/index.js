62da7a76d6ca4455df4decb70c764d3bba1ebf02
Alex Kinsey <alex.kinsey@lloydsbanking.com>
2022-06-15 14:52:05 +0100

    connected server to database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
-const express = require("express");
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
+const db = new sqlite3.Database(__dirname + '/database/concierge.db');
+// db.each('SELECT * FROM customers;', function (err, row) {
+//   console.log(row);
+// });
+
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-})
+  console.log(`Server listening on ${PORT}`);
+});
+
+app.get('/api/customers', (req, res) => {
+  db.all('SELECT * FROM customers;', (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
