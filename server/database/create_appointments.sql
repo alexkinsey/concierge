81d47ebbaeb3bee7f7b328cb7b18429ca60c26a7
Alex Kinsey <alex.kinsey@lloydsbanking.com>
2022-06-23 11:23:48 +0100

    new columns in appointment table

--- server/database/create_appointments.sql
+++ server/database/create_appointments.sql
@@ -4,8 +4,13 @@ DROP TABLE IF EXISTS appointments;
 
 CREATE TABLE appointments(
     appointmentId INTEGER PRIMARY KEY,
+    businessArea VARCHAR(20) NOT NULL,
+    purpose TEXT NOT NULL,
+    location VARCHAR(15) NOT NULL,
+    branch VARCHAR(50),
     date DATE NOT NULL,
     time TIME NOT NULL,
+    comments TEXT,
     customerId INTEGER,
     consultantId INTEGER,
     FOREIGN KEY (customerId) REFERENCES customers(customerId),
